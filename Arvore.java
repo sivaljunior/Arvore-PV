f8056716bc823990aea34ecb4f25ed43d157ef62
Marli <abc@hotmail.com>
2015-05-26 08:18:25 -0300

    Meudeus, compila

--- Arvore.java
+++ Arvore.java
@@ -76,7 +76,94 @@ class Arvore{
 		raiz.cor = false;		
 	}
 	
+	public void transplant(Nodo u, Nodo v){
+		if(u.pai == null) raiz = v;
+		else if(u == u.pai.esq) u.pai.esq = v;
+		else u.pai.dir = v;
+		v.pai = u.pai;
+	}
 	
+	public void delete(Nodo z){
+		Nodo y = z, x;
+		boolean yCorOriginal = y.cor;
+		if(z.esq == null){
+			x = z.dir;
+			transplant(z, z.dir);
+		} else if(z.dir == null){
+			x = z.esq;
+			transplant(z, z.esq);
+		} else {
+			y = z.dir.minimo();
+			yCorOriginal = y.cor;
+			x = y.dir;
+			if(y.pai == z) x.pai = y;
+			else{
+				transplant(y, y.dir);
+				y.dir = z.dir;
+				y.dir.pai = y;
+			}
+			transplant(z, y);
+			y.esq = z.esq;
+			y.esq.pai = y;
+			y.cor = z.cor;
+		}
+		if(yCorOriginal == false) delFix(x);
+	}
 	
+	public void delFix(Nodo x){
+		Nodo w;
+		while(x != raiz && x.cor == false){
+			if(x == x.pai.esq){
+				w = x.pai.dir;
+				if(w.cor){
+					w.cor = false;
+					x.pai.cor = true;
+					left_rotate(x.pai);
+					w = x.pai.dir;
+				}
+				if(w.esq.cor == false && w.dir.cor == false){
+					w.cor = true;
+					x = x.pai;
+				} else {
+					if(w.dir.cor == false){
+						w.esq.cor = false;
+						w.cor = true;
+						right_rotate(w);
+						w = x.pai.dir;
+					}
+				}
+				w.cor = x.pai.cor;
+				x.pai.cor = false;
+				w.dir.cor = false;
+				left_rotate(x.pai);
+				x = raiz;
+			} else {
+				w = x.pai.esq;
+				if(w.cor){
+					w.cor = false;
+					x.pai.cor = true;
+					right_rotate(x.pai);
+					w = x.pai.esq;
+				}
+				if(w.dir.cor == false && w.esq.cor == false){
+					w.cor = true;
+					x = x.pai;
+				} else {
+					if(w.esq.cor == false){
+						w.dir.cor = false;
+						w.cor = true;
+						left_rotate(w);
+						w = x.pai.esq;
+					}
+				}
+				w.cor = x.pai.cor;
+				x.pai.cor = false;
+				w.esq.cor = false;
+				right_rotate(x.pai);
+				x = raiz;
+			}
+		}
+		x.cor = false;
+	}
 	
 }
